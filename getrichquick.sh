8e173aa4f28e1b0174c0e08d5f995912ec64ed71
sath000007 <101625652+cicada0007@users.noreply.github.com>
2022-11-04 01:50:27 +0530

    Add files via upload

---
+++ getrichquick.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+read name
+read age
+
+echo "Hello $name you are $age years old"
+echo "$PWD,$HOST,$PWD,$SHELL"
+echo "$name"
+echo "$cyber"
+echo "calucluting....."
+sleep 2
+
+getrich=$((($RANDOM % 19)+$age))
+echo "you will become billinare at this age $getrich"
